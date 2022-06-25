287160f7bb9aa1b699d5aa5570ce5b79892e5048
indiigo-o <edin.gondzo@edu.fit.ba>
2022-06-25 22:46:22 +0200

    edit song

--- Frontend/SRFrontend/src/app/edit-song/edit-song.component.ts
+++ Frontend/SRFrontend/src/app/edit-song/edit-song.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-song',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-song.component.css']
 })
 export class EditSongComponent implements OnInit {
+  sName: any;
+  sArtist: any;
+  sUrl: any;
+  sLenght: any;
+  sCategory: any;
+  song:any;
 
-  constructor() { }
+  songID:any= Number(this.route.snapshot.paramMap.get('id'));
+
+  Categories:any;
+  
+  constructor(private httpKlijent: HttpClient,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.LoadSong();
+    this.LoadCategorys();
   }
 
+  btnAddSong() {
+
+    console.log(this.song)
+  }
+
+  LoadCategorys(){
+    this.httpKlijent.get("https://localhost:44308/SongCategory/GetAll")
+    .subscribe(x=>{
+      console.log("Categorys", x);
+      this.Categories = x;
+    });
+  }
+  LoadSong(){
+    
+    this.httpKlijent.get("https://localhost:44308/Song/Get/"+this.songID)
+    .subscribe(x=>{
+      console.log("Song", x);
+      this.song = x;
+    });
+
+  }
 }
